f41bc912b3bdfdb92e35229d025dc849d0f3f7f1
Ngdustry <Ngdustry@gmail.com>
2022-07-18 18:38:06 -0400

    feat: add db class

---
+++ config/Database.php
@@ -0,0 +1,31 @@
+<?php
+
+class Database
+{
+  private $connection;
+
+  public function __construct()
+  {
+    $this->host = getenv('HOST');
+    $this->dbname = getenv('DBNAME');
+    $this->username = getenv('USERNAME');
+    $this->password = getenv('PASSWORD');
+  }
+
+  public function connect()
+  {
+    $this->connection = null;
+
+    try {
+      $dsn = "mysql:host={$this->host};dbname={$this->dbname}";
+
+      $this->connection = new PDO($dsn, $this->username, $this->password);
+
+      $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $error) {
+      echo "Connection error: {$error->getMessage()}";
+    }
+
+    return $this->connection;
+  }
+}
